5c86c39c8dab88a4f428d03ea2bd885a1d7c474f
Brian Lau <blths2k7@gmail.com>
2014-02-14 01:00:48 -0800

    configure settings for images

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,9 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "default.jpg", :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "default.jpg"
+	else
+	  has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "default.jpg", :storage => :dropbox,
+	    :dropbox_credentials => Rails.root.join("config/dropbox.yml"), :path => ":style/:id_:filename"
+	end
   #validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
